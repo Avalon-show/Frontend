e1208528904068fed2f21396e12b6026bef76640
Ramiro Contreras León <racramiro@gmail.com>
2020-09-29 17:08:34 -0500

    Agregando formulario de Login

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,22 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles/LoginComponent.css";
 
 class LoginComponent extends React.Component {
   render() {
     return (
-      <form className="w-full max-w-sm">
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="inline-full-name"
-            >
-              Full Name
-            </label>
+      <div className="justify-center contenedor px-12">
+        <div className="flex flex-col items-center contenedor-caja">
+          <h1 className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 text-white text-4xl m-10 text-center">
+            Iniciar Sesión
+          </h1>
+          <input
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 leading-snug m-2 py-4 px-4 bg-gray-400 rounded placeholder-black hover:bg-white focus:outline-none focus:shadow-outline"
+            type="Email"
+            placeholder="Email"
+          ></input>
+          <input
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 leading-snug m-2 py-4 px-4 bg-gray-400 rounded placeholder-black hover:bg-white  focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Contraseña"
+          ></input>
+          <Link
+            to="/Main"
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 button-login items-center mt-10"
+          >
+            Iniciar Sesión
+          </Link>
+
+          <div className="flex w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 justify-between">
+            <h6 className=" text-white text-sm text-left mt-2 mb-8">
+              Recuerdame
+            </h6>
+            <h6 className=" text-white text-sm text-right mt-2 mb-8">
+              ¿Necesitas ayuda?
+            </h6>
+          </div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 mt-2 mb-10">
+            <h3 className=" text-white text-base mt-2 mb-2 ">
+              ¿Primera vez en Netflix? Suscríbete ya.
+            </h3>
+            <h3 className="text-white text-sm mt-2 mb-2">
+              Esta página está protegida por Google reCAPTCHA para comprobar que
+              no eres un robot. Más info.
+            </h3>
           </div>
         </div>
-      </form>
-    )
+      </div>
+    );
   }
 }
 
